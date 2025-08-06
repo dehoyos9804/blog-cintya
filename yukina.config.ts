2b4ee3fa9203d30fed9a9cf356d37c39a01f61ce
Aldair De Hoyos <aldair.dehoyos@servinformacion.com>
2025-08-06 15:04:21 -0500

    🔧 feat: init commit initial 7

--- yukina.config.ts
+++ yukina.config.ts
@@ -3,7 +3,7 @@ import type { Configuration } from "./src/types/config";
 
 const YukinaConfig: Configuration = {
   title: "StoryTelling",
-  subTitle: "Incidencia del storytelling en el desarrollo de habilidades comunicativas del idioma inglés en estudiantes de sexto grado de la Institución Educativa Técnica Pio XII",
+  subTitle: "Incidencia del storytelling en el desarrollo de habilidades comunicativas del idioma inglés en estudiantes de decimo grado de la Institución Educativa Técnica Pio XII",
   brandTitle: "StoryTelling",
 
   description: "Demo Site",
